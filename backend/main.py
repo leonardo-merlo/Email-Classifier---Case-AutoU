1ca44978647966cd6cf3754dbc85218d1fc969e0
leonardo-merlo <leomamerlo@gmail.com>
2025-09-28 21:04:53 -0300

    fix: updated netlify frontend endpoint

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://email-classifier-autou.netlify.app/"
+        "https://email-classifier-autou.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
